ed69111f7b9bd5c6ec2dc22220f550cb02a95580
林建 <linjian@ling.ai>
2017-02-09 15:37:13 +0800

    Function Add: Message send (50%) . Monitor message sending and receiving.

--- IMDemo/ChatViewController.swift
+++ IMDemo/ChatViewController.swift
@@ -14,26 +14,66 @@ class ChatViewController: UIViewController {
     
     var user: NIMUser?
     @IBOutlet weak var titleItem: UINavigationItem!
-    
+    @IBOutlet weak var inputTextField: UITextField!
     
     // MARK: - LifeCycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        NIMSDK.shared().chatManager.add(self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        NIMSDK.shared().chatManager.add(self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.titleItem.title = user?.userId
     }
     
+    deinit {
+        NIMSDK.shared().chatManager.remove(self)
+    }
     
     // MARK: - Events
     
     @IBAction func sendButtonPressed(_ sender: UIButton) {
         let message = NIMMessage()
+        message.text = inputTextField.text
+        //清空textField
+        self.inputTextField.text = ""
         let session = NIMSession((user?.userId)!, type: NIMSessionType.P2P)
         do {
            try NIMSDK.shared().chatManager.send(message, to: session)
         } catch {
             print(error)
         }
+        print(message.description)
     }
     
+}
+
+extension ChatViewController: NIMChatManagerDelegate {
+    func onRecvMessages(_ messages: [NIMMessage]) {
+        for message in messages {
+            print(message.text as Any)
+        }
+    }
+    
+    func send(_ message: NIMMessage, didCompleteWithError error: Error?) {
+        if error == nil {
+            let alert = UIAlertController(title: "提示", message: "发送成功！", preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+        else {
+            let alert = UIAlertController(title: "提示", message: "发送失败,\(error)", preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+    }
 }
